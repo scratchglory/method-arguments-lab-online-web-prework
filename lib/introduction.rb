b50720db723a6a788516a9b3915b36932c6710d9
Adella <adellakenn@gmail.com>
2019-04-22 21:31:36 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,3 +4,10 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
+
+#define a method, #introduction_with_language that takes two argumetns (name, language)
+# output "Hi, my name is #{name} and I am learning to program #{language}."
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
